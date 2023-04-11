976572301d3ca80c2b4ad0cbd0e37820db13f73b
Victor <victorqalves@gmail.com>
2023-04-11 11:10:49 -0400

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coach } from './typeOrm/entities/Coach';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'genfit_dev',
+      entities: [Coach],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
