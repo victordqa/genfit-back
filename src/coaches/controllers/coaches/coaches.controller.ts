018dc3269b849f0c7231160d6916b2a1b77e12c3
Victor <victorqalves@gmail.com>
2023-04-11 14:40:50 -0400

    basic create coach logic

--- src/coaches/controllers/coaches/coaches.controller.ts
+++ src/coaches/controllers/coaches/coaches.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCoachDto } from 'src/coaches/dtos/CreateCoach.dto';
+import { CoachesService } from 'src/coaches/services/coaches/coaches.service';
 
 @Controller('coaches')
 export class CoachesController {
+  constructor(private coachesService: CoachesService) {}
   @Get()
   findCoaches() {}
 
-  @Post()
-  createCoach() {}
+  @Post('create')
+  async createCoach(@Body() createCoachDto: CreateCoachDto) {
+    const createdCoach = await this.coachesService.createCoach(createCoachDto);
+    return { id: createdCoach.id };
+  }
 }
--- src/coaches/services/coaches/coaches.service.ts
+++ src/coaches/services/coaches/coaches.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Coach } from 'src/typeOrm/entities/Coach';
+import { CreateCoachParams } from 'src/utils/types/types';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,4 +9,14 @@ export class CoachesService {
   constructor(
     @InjectRepository(Coach) private coachRepository: Repository<Coach>,
   ) {}
+
+  createCoach(coachDetails: CreateCoachParams) {
+    const newCoach = this.coachRepository.create({
+      ...coachDetails,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    return this.coachRepository.save(newCoach);
+  }
 }
