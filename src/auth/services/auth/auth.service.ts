7a72a86922a93a34ca1844c0ded550a55242b5ed
Victor <victorqalves@gmail.com>
2023-04-17 19:06:34 -0400

    basic passport wiring

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { CoachesService } from 'src/coaches/services/coaches/coaches.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('COACHES_SERVICE') private readonly coachesService: CoachesService,
+  ) {}
+  async validateCoach(email: string, password: string) {
+    const dbCoach = await this.coachesService.findCoachByEmail(email);
+
+    if (!dbCoach || password !== dbCoach.password) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return {};
+  }
+}
