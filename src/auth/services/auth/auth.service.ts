6a23ed4395d730344ebc54f7ede2e76456fa300f
Victor <victorqalves@gmail.com>
2023-04-18 12:13:22 -0400

    simple injectioninstead of using token

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -3,9 +3,7 @@ import { CoachesService } from 'src/coaches/services/coaches/coaches.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject('COACHES_SERVICE') private readonly coachesService: CoachesService,
-  ) {}
+  constructor(private coachesService: CoachesService) {}
   async validateCoach(email: string, password: string) {
     const dbCoach = await this.coachesService.findCoachByEmail(email);
 
