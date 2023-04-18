eb525e38395a4e48f4b4bdc7d31048025384412b
Victor <victorqalves@gmail.com>
2023-04-18 12:12:51 -0400

    prin successfullt logged user

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Request, Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { SignInDto } from 'src/auth/dtos/SignInDto';
 import { AuthService } from 'src/auth/services/auth/auth.service';
+import { LocalAuthGuard } from 'src/auth/utils/LocalAuthGuard';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('sign-in')
-  async signIn(@Body() signInDto: SignInDto) {
-    console.log('from controller', signInDto);
-    const { email, password } = signInDto;
-    return { message: 'logged in' };
+  async signIn(@Request() req) {
+    return req.user;
   }
 }
