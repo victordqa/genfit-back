30242724744ff77a2167122b70743b693b23790a
Victor <victorqalves@gmail.com>
2023-08-01 15:17:35 -0400

    cookie setup

--- src/auth/utils/JwtStrategy.ts
+++ src/auth/utils/JwtStrategy.ts
@@ -1,15 +1,29 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET_KEY,
     });
+  }
+
+  private static extractJWT(req: Request): string | null {
+    if (
+      req?.cookies?.accessToken?.access_token &&
+      req.cookies.accessToken.access_token.length > 0
+    ) {
+      return req.cookies.accessToken.access_token;
+    }
+    return null;
   }
 
   async validate(payload: any) {
