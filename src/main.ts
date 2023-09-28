a61501f94d40fa49ab31e19977f09987a04576a6
Victor <victorqalves@gmail.com>
2023-09-28 19:15:06 -0400

    prod server cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3000',
+      'https://genfit-front-mermf4p0y-victordqa.vercel.app',
+    ],
     credentials: true,
   });
 
